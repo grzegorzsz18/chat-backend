f0892dc2bf3ffc7600dad3d011b4741b56cbf943
grzegorz <grzegorz.szczepaniak18@gmail.com>
2018-02-17 19:11:13 +0100

    script for token tables

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS oauth_access_token;
+DROP TABLE IF EXISTS oauth_refresh_token;
+
+CREATE TABLE oauth_access_token (
+  token_id VARCHAR(255),
+  token LONG BYTE,
+  authentication_id VARCHAR(255) PRIMARY KEY,
+  user_name VARCHAR(255),
+  client_id VARCHAR(255),
+  authentication LONG BYTE,
+  refresh_token VARCHAR(255) );
+
+CREATE TABLE oauth_refresh_token (
+  token_id VARCHAR(255),
+  token LONG BYTE,
+  authentication LONG BYTE );
