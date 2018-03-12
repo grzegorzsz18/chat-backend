e7227fee5bbb4d2f488e58f14b4e25327d632e53
grzegorz <grzegorz.szczepaniak18@gmail.com>
2018-03-12 14:20:44 +0100

    sending messages

--- src/main/java/pl/szczepaniak/chat/controller/MessageController.java
+++ src/main/java/pl/szczepaniak/chat/controller/MessageController.java
@@ -3,11 +3,9 @@ package pl.szczepaniak.chat.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.szczepaniak.chat.exceptions.ConversationNotFoundException;
+import pl.szczepaniak.chat.exceptions.UserNotFoundException;
 import pl.szczepaniak.chat.service.MessageService;
 import pl.szczepaniak.chat.service.dto.MessageDTO;
 
@@ -29,6 +27,14 @@ public class MessageController {
                                                         @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
                                                         @RequestParam(value = "limit" , required = false, defaultValue = "10") Integer limit) throws ConversationNotFoundException {
         return new ResponseEntity<>(messageService.getAllByConversation(id, page, limit), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity sendMessage(@RequestBody String textMessage,
+                                      @RequestParam(value = "userEmail") String userEmail,
+                                      @RequestParam(value = "conversationId") Long conversationId) throws UserNotFoundException, ConversationNotFoundException {
+        messageService.sendMessage(textMessage, userEmail, conversationId);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
 
--- src/main/java/pl/szczepaniak/chat/service/MessageService.java
+++ src/main/java/pl/szczepaniak/chat/service/MessageService.java
@@ -5,10 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import pl.szczepaniak.chat.exceptions.ConversationNotFoundException;
+import pl.szczepaniak.chat.exceptions.UserNotFoundException;
 import pl.szczepaniak.chat.model.ConversationRepositoryCRUD;
 import pl.szczepaniak.chat.model.MessageRepositoryCRUD;
+import pl.szczepaniak.chat.model.UserRepositoryCRUD;
 import pl.szczepaniak.chat.model.entity.Conversation;
 import pl.szczepaniak.chat.model.entity.Message;
+import pl.szczepaniak.chat.model.entity.User;
 import pl.szczepaniak.chat.service.converter.MessageToDtoConverter;
 import pl.szczepaniak.chat.service.dto.MessageDTO;
 
@@ -21,12 +24,15 @@ public class MessageService {
 
     MessageRepositoryCRUD messageRepositoryCRUD;
     ConversationRepositoryCRUD conversationRepositoryCRUD;
+    UserRepositoryCRUD userRepositoryCRUD;
 
     @Autowired
     public MessageService(MessageRepositoryCRUD messageRepositoryCRUD,
-                          ConversationRepositoryCRUD conversationRepositoryCRUD) {
+                          ConversationRepositoryCRUD conversationRepositoryCRUD,
+                          UserRepositoryCRUD userRepositoryCRUD) {
         this.messageRepositoryCRUD = messageRepositoryCRUD;
         this.conversationRepositoryCRUD = conversationRepositoryCRUD;
+        this.userRepositoryCRUD = userRepositoryCRUD;
     }
 
     public List<MessageDTO> getAllByConversation(Long conversationId, Integer page, Integer limit) throws ConversationNotFoundException {
@@ -44,5 +50,26 @@ public class MessageService {
                 .map(MessageToDtoConverter::convert)
                 .collect(Collectors.toList());
 
+    }
+
+
+    public void sendMessage(String text, String userEmail, long conversationId ) throws UserNotFoundException, ConversationNotFoundException {
+        Optional<User> user = userRepositoryCRUD.getUserByEmail(userEmail);
+        if(!user.isPresent()){
+            throw new UserNotFoundException();
+        }
+        Optional<Conversation> conversation = conversationRepositoryCRUD.findOneById(conversationId);
+        if(!conversation.isPresent()){
+            throw new ConversationNotFoundException();
+        }
+        messageRepositoryCRUD.save(
+                Message.builder()
+                .conversation(conversation.get())
+                .isDisplayed(false)
+                .text(text)
+                .user(user.get())
+                .time(System.currentTimeMillis())
+                .build()
+        );
     }
 }
